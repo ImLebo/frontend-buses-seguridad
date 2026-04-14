32532df61dd3f0831a7e6595b279e00b5026e925
joaquinK840 <joeshoca@gmail.com>
2026-04-14 10:43:17 -0500

    2

--- src/pages/PasswordRecoveryPage.tsx
+++ src/pages/PasswordRecoveryPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { PasswordRecoveryForm } from '../components/auth';
+import { PasswordRecoveryForm } from '../components/auth/PasswordRecoveryForm';
 import { usePasswordRecovery } from '../hooks/usePasswordRecovery';
 
 export const PasswordRecoveryPage = () => {
